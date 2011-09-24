52dd28dfd01224a5673ec5509122282f364e7843
Rafael Fernández López <ereslibre@kde.org>
2011-09-24 02:18:42 +0200

    Modify permissions

--- documentrepository/protected/models/User.php
+++ documentrepository/protected/models/User.php
@@ -97,7 +97,7 @@ class User extends CActiveRecord
 
 	private function generateKey($length = 16) {
 		$key = '';
-		for($i = 0; $i < $length; $i ++) {
+		for ($i = 0; $i < $length; $i++) {
 			$key .= chr(mt_rand(33, 126));
 		}
 		return $key;
