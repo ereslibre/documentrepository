faa17f0fcb398347b159ec2bf63551b0b726a9d3
Rafael Fernández López <ereslibre@kde.org>
2011-09-24 05:03:07 +0200

    Javascript is now smarter when adding characters, but still needs to be improved a lot

--- documentrepository/js/document.js
+++ documentrepository/js/document.js
@@ -1,17 +1,42 @@
 var i = 0;
+var available_characters = new Array();
+
+function cloneCharacters(i)
+{
+	var characters = $('#characters').clone();
+	return characters;
+}
 
 function addCharacter()
 {
 	if (i > 0) {
+		var selected = $('#character' + (i - 1)).val();
+		var removeMe;
+		$.each(available_characters, function(i, v) {
+			if (v == selected) {
+				removeMe = i;
+			}
+		});
+		available_characters.splice(removeMe, 1);
+		$('#character' + (i - 1)).hide();
 		$('#addcharacter' + (i - 1)).hide();
+		$('#removecharacter' + (i - 1)).show();
+		$('#characterlabel' + (i - 1)).html($('#character' + (i - 1) + ' option:selected').text());
+		$('#characterlabel' + (i - 1)).show();
 	}
-	var character = $('#characters').clone();
+
+	if (!available_characters.length) {
+		return;
+	}
+
+	var character = cloneCharacters(i);
 	character.attr('id', 'character' + i);
 	character.attr('name', 'character' + i);
 
 	var character_wrapper = $('<div></div>');
 	character_wrapper.append(character);
-	character_wrapper.append('Remove');
+	character_wrapper.append('<div id="characterlabel' + i + '"></div>');
+	character_wrapper.append('<a style="display: none;" id="removecharacter' + i + '" onclick="removeCharacter();" href="javascript:void(0);">Remove</a>');
 	character_wrapper.append('<a id="addcharacter' + i + '" onclick="addCharacter();" href="javascript:void(0);">Add</a>');
 
 	$('#selectedcharacters').append(character_wrapper);
@@ -19,5 +44,8 @@ function addCharacter()
 }
 
 $(document).ready(function() {
+	$('#characters option').each(function() {
+		available_characters.push($(this).val());
+	});
 	addCharacter();
 });
