f673e8cd52e8fb758f2cedbb187d7115d1589ceb
Rafael Fernández López <ereslibre@kde.org>
2011-10-05 22:01:32 +0200

    Easily select dates :)

---
+++ documentrepository/js/character.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#Character_birth_date").datepicker({
+		changeMonth: true,
+		changeYear: true
+	});
+
+	$("#Character_death_date").datepicker({
+		changeMonth: true,
+		changeYear: true
+	});
+});
