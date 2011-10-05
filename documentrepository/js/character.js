9c7bccbb82846f22e42065480f81dda4f4bdf02f
Rafael Fernández López <ereslibre@kde.org>
2011-10-06 01:04:25 +0200

    Add date format

--- documentrepository/js/character.js
+++ documentrepository/js/character.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+	var currentTime = new Date();
+
 	$("#Character_birth_date").datepicker({
 		changeMonth: true,
-		changeYear: true
+		changeYear: true,
+		dateFormat: "dd/mm/yy"
 	});
 
 	$("#Character_death_date").datepicker({
 		changeMonth: true,
-		changeYear: true
+		changeYear: true,
+		dateFormat: "dd/mm/yy"
 	});
 });
