5696458c834a829a0282515eee4b9db6be7f91d6
Rafael Fernández López <ereslibre@kde.org>
2011-10-06 01:44:32 +0200

    Add event.js for event view

---
+++ documentrepository/js/event.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	$("#Event_start_date").datepicker({
+		changeMonth: true,
+		changeYear: true,
+		dateFormat: "dd/mm/yy"
+	});
+
+	$("#Event_end_date").datepicker({
+		changeMonth: true,
+		changeYear: true,
+		dateFormat: "dd/mm/yy"
+	});
+});
