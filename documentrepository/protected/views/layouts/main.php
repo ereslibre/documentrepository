3004ebce7bf99b0226a55d980133891c4b8a2dfe
Rafael Fernández López <ereslibre@kde.org>
2011-10-13 03:18:04 +0200

    No footer at all

--- documentrepository/protected/views/layouts/main.php
+++ documentrepository/protected/views/layouts/main.php
@@ -62,12 +62,6 @@
 
 	<?php echo $content; ?>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
 </div><!-- page -->
 
 </body>
