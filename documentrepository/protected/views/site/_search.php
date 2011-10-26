1d9087b005123ae664941e3441a17207272532c0
Rafael Fernández López <ereslibre@kde.org>
2011-10-26 11:52:30 +0200

    Include _search partial

---
+++ documentrepository/protected/views/site/_search.php
@@ -0,0 +1,12 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - Search';
+$this->breadcrumbs=array(
+	'Search',
+);
+?>
+
+<h1>Search</h1>
+
+<p>Enter your search terms:</p>
+
+Search terms here
--- documentrepository/protected/views/site/index.php
+++ documentrepository/protected/views/site/index.php
@@ -1,7 +1,2 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
-
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-Here you will soon find a powerful search engine :)
-<br/><br/>
-In the meanwhile you can explore from the actions on the menu
+<?php echo $this->renderPartial('_search'); ?>
