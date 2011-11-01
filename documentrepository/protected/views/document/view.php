ac4064f6827a918970257a18620a9f5ecd1aa3de
Rafael Fernández López <ereslibre@ereslibre.es>
2011-11-01 18:27:09 +0100

    Render partial here, we need to reuse as much code as possible

--- documentrepository/protected/views/document/view.php
+++ documentrepository/protected/views/document/view.php
@@ -15,11 +15,4 @@ $this->menu=array(
 
 <h1>View Document #<?php echo $model->id; ?></h1>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'document',
-		'name',
-	),
-)); ?>
+<?php echo $this->renderPartial('_view', array('data'=>$model,)); ?>
